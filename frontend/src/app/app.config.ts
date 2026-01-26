1cbb809aa4ef8422a61d6aac0cf263aec3c91b3f
Yahya Elmami <yahyaelmami9@gmail.com>
2026-01-26 20:01:23 +0000

    Force refresh of translation files

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -18,7 +18,7 @@ export class CustomTranslateLoader implements TranslateLoader {
   constructor(private http: HttpClient) { }
   getTranslation(lang: string): Observable<any> {
     // Using absolute path to ensure translations load on sub-routes
-    return this.http.get(`/assets/i18n/${lang}.json`);
+    return this.http.get(`/assets/i18n/${lang}.json?v=${new Date().getTime()}`);
   }
 }
 
