48ebe446d52d37d4b89cb9b725971764a0fffa1e
Yahya Elmami <yahyaelmami9@gmail.com>
2026-01-30 23:12:43 +0000

    Fix: Logic error in db.js (add try block)

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,9 +4,10 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    throw error;
   }
 };
 
